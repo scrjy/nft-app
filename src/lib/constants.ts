7105a694e563d0eb3d0bcd5b9e3ff17ba846a9c2
scrjy <sjyplus123@naver.com>
2025-11-18 10:52:20 +0900

    1st

---
+++ src/lib/constants.ts
@@ -0,0 +1,15 @@
+export const contractAddress = '0x4c58d95361d99EA6a53B00FAb7f43fb73dF2ce52'
+
+export const SEPOLIA_CHAIN_ID = 11155111
+
+export const SEPOLIA_NETWORK = {
+  chainId: `0x${SEPOLIA_CHAIN_ID.toString(16)}`,
+  chainName: 'Sepolia',
+  nativeCurrency: {
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: ['https://sepolia.infura.io/v3/'],
+  blockExplorerUrls: ['https://sepolia.etherscan.io/'],
+}
